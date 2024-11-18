19834c7c00133649ce484f95430cebc6e69c71f9
Folgert Karsdorp <fbkarsdorp@gmail.com>
2024-11-18 16:33:05 +0100

    updates

--- tex/auto/main.el
+++ tex/auto/main.el
@@ -6,14 +6,14 @@
    (TeX-add-to-alist 'LaTeX-provided-class-options
                      '(("report" "12pt" "letterpaper")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
-                     '(("fontenc" "T1") ("csquotes" "strict" "autostyle") ("babel" "USenglish") ("biblatex" "backend=biber" "style=apa" "maxcitenames=2" "maxbibnames=99" "sorting=ydnt" "uniquelist=false" "mincitenames=1" "citestyle=authoryear")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+                     '(("fontenc" "T1") ("csquotes" "strict" "autostyle") ("babel" "USenglish") ("biblatex" "backend=biber" "style=apa" "maxcitenames=2" "maxbibnames=99" "sorting=ydnt" "uniquelist=false" "mincitenames=1" "citestyle=authoryear") ("xcolor" "svgnames")))
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
@@ -33,6 +33,7 @@
     "ebgaramond"
     "helvet"
     "biblatex"
+    "xcolor"
     "authblk")
    (TeX-add-symbols
     '("namefont" 1)
